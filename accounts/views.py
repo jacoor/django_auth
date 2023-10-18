7e2c82e655dd31c4a25a2741489e1dec3a09ea7f
Jacek Ostański <jacek@ivolution.pl>
2023-10-18 12:40:07 +0200

    registratin works

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import RegistrationForm
 
-# Create your views here.
+
+class RegistrationView(CreateView):
+    template_name = "registration/registration.html"
+    form_class = RegistrationForm
+    success_url = reverse_lazy("home")  # Przekierowanie po udanej rejestracji
+
+    def form_valid(self, form):
+        user = form.save()
+        username = form.cleaned_data.get("username")
+        raw_password = form.cleaned_data.get("password")
+        user = authenticate(username=username, password=raw_password)
+        login(self.request, user)
+        return super().form_valid(form)
