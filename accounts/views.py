c5f7d1231b0c44c5e18ca1b20ff3122cd19181d2
Jacek Ostański <jacek@ivolution.pl>
2023-10-25 11:26:33 +0200

    fix registration

--- accounts/views.py
+++ accounts/views.py
@@ -7,12 +7,15 @@ from .forms import RegistrationForm
 class RegistrationView(CreateView):
     template_name = "registration/registration.html"
     form_class = RegistrationForm
-    success_url = reverse_lazy("home")  # Przekierowanie po udanej rejestracji
+    success_url = reverse_lazy(
+        ""
+    )  # Przekierowanie po udanej rejestracji. Tak, puste. Ćwiczenie dla Was.
 
     def form_valid(self, form):
+        valid = super().form_valid(form)
         user = form.save()
         username = form.cleaned_data.get("username")
         raw_password = form.cleaned_data.get("password")
         user = authenticate(username=username, password=raw_password)
         login(self.request, user)
-        return super().form_valid(form)
+        return valid
